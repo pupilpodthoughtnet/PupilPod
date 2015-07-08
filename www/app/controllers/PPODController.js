f39f3247f4ad626ca5d538229f9fb32a991d5bb0
pupilpodthoughtnet <pupilpodthoughtnet@gmail.com>
2015-07-08 15:29:09 +0530

    Commit 10
    Commit 10

--- www/app/controllers/PPODController.js
+++ www/app/controllers/PPODController.js
@@ -86,6 +86,11 @@ app.controller('PPODController',function($scope,PPODService,$window,$rootScope,$
           // this is the actual push notification. its format depends on the data model from the push server
 		  PPODService.AddNotificationToDB($scope,notification);
 			$cordovaDialogs.alert(notification.message, "Push Notification Received");
+			alert('NG '+notification.notify_guid);
+			alert('ND '+notification.notify_date);
+			alert('NT '+notification.notify_type);
+			alert('NM '+notification.notify_msg);
+			alert('EG '+notification.entity_guid);
 			//alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
 			break;
 
